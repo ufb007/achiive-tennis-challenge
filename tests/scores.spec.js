706ee0fdaed151ac365656907ff59a241b988941
Ufuk Bozdemir <ufb007@gmail.com>
2024-02-01 15:49:22 +0000

    Scores update

--- tests/scores.spec.js
+++ tests/scores.spec.js
@@ -58,6 +58,24 @@ describe('test scoring', () => {
             winner: '',
             message: 'Game Point',
             points: [3, 2]
-        })
+        });
+    });
+
+    test('test winner', () => {
+        const [player1, player2] = players;
+
+        player1.points = 4;
+        player1.server = true;
+        player2.points = 2;
+
+        const response = CalculateScores(players);
+
+        expect(response).toEqual({
+            finished: true,
+            winner: '',
+            winner: 'Roger Federer',
+            message: 'Game Point',
+            points: [4, 2]
+        });
     });
 });
